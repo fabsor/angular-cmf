8734c43fbb9c028d138e4b519affcd216e59e0a7
Fabian Sorqvist <fabian@sorqvist.nu>
2013-02-13 22:03:47 +0100

    Wordpress example app

---
+++ src/wordpress-example/project.js
@@ -0,0 +1,35 @@
+'use strict'
+var angular;
+
+// Specify your base url here.
+var backendBaseUrl = 'http://192.168.50.2/angular-cmf/index.php';
+
+var project = angular.module("project", ["cmf.user", "ngResource", "cmf.content"]);
+
+project.config(function ($routeProvider) {
+  $routeProvider
+    .when('/login', { controller: 'Login', template: '<status logger="logger"></status><login login="login"></login>' })
+    .when('/', { controller: 'User', templateUrl: "../modules/user/templates/user-admin.html" });
+});
+
+project.controller('User', function ($scope, UserService, Logger, UserAdmin, SessionService) {
+  var session = SessionService.getSession();
+  if (!session) {
+    // Set up backend url.
+    var User = UserService(backendBaseUrl);
+    UserAdmin(User, $scope);
+  }
+});
+
+project.controller('Login', function ($scope, LoginService, Logger, SessionService) {
+  var session = SessionService.getSession();
+  var Login = LoginService(backendBaseUrl);
+  $scope.logger = new Logger();
+  $scope.login = function (username, password) {
+    Login.login(username, password, function (token) {
+      SessionService.createSession(token);
+    }, function (errors) {
+      $scope.logger.addMessage("error", errors);
+    });
+  };
+});
