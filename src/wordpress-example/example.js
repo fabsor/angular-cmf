595eb88ec85db49c0bd941db1f7cc165ce5ca535
Fabian Sorqvist <fabian@sorqvist.nu>
2013-02-16 17:30:45 +0100

    Expand the wordpress example app

--- src/wordpress-example/example.js
+++ src/wordpress-example/example.js
@@ -9,17 +9,33 @@ var project = angular.module("project", ["cmf.user", "ngResource"]);
 project.config(function ($routeProvider) {
   $routeProvider
     .when('/login', { controller: 'Login', template: '<status logger="logger"></status><login login="login"></login>' })
-    .when('/', { controller: 'User', templateUrl: "../modules/user/templates/user-admin.html" });
+    .when('/', { controller: 'User', templateUrl: "../modules/user/templates/user-admin.html" })
+    .when('/user/add', { controller: 'UserAdd', templateUrl: "../modules/user/templates/user-add.html" });
 });
 
-project.controller('User', function ($scope, UserService, Logger, UserAdmin, SessionService) {
+project.controller('UserAdd', function ($scope, UserService, TypeService, UserAdd, SessionService) {
   var session = SessionService.getSession();
   if (!session) {
-    location.path('/');
+    $location.path('/login');
   }
-  // Set up backend url.
-  var User = UserService(backendBaseUrl);
-  UserAdmin(User, $scope);
+  else {
+    // Set up backend url.
+    var Types = TypeService(backendBaseUrl);
+    var User = UserService(backendBaseUrl);
+    UserAdd(User, Types, $scope);
+  }
+});
+
+project.controller('User', function ($scope, UserService, Logger, UserAdmin, SessionService, $location) {
+  var session = SessionService.getSession();
+  if (!session) {
+    $location.path('/login');
+  }
+  else {
+    // Set up backend url.
+    var User = UserService(backendBaseUrl);
+    UserAdmin(User, $scope);
+  }
 });
 
 project.controller('Login', function ($scope, LoginService, Logger, SessionService, $location) {
@@ -27,13 +43,16 @@ project.controller('Login', function ($scope, LoginService, Logger, SessionServi
   if (session) {
     $location.path('/');
   }
-  var Login = LoginService(backendBaseUrl);
-  $scope.logger = new Logger();
-  $scope.login = function (username, password) {
-    Login.login(username, password, function (token) {
-      SessionService.createSession(token);
-    }, function (errors) {
-      $scope.logger.addMessage("error", errors);
-    });
-  };
+  else {
+    var Login = LoginService(backendBaseUrl);
+    $scope.logger = new Logger();
+    $scope.login = function (username, password) {
+      Login.login(username, password, function (token) {
+        SessionService.createSession(token);
+        $location.path('/');
+      }, function (errors) {
+        $scope.logger.addMessage("error", errors);
+      });
+    };
+  }
 });
