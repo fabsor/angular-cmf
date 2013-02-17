6cc2f79794e26f621068b4c4b7e42d3b68e8e975
Fabian Sorqvist <fabian@sorqvist.nu>
2013-02-17 16:20:15 +0100

    Pass logger to user view function

--- src/wordpress-example/example.js
+++ src/wordpress-example/example.js
@@ -4,31 +4,31 @@ var angular;
 // Specify your base url here.
 var backendBaseUrl = 'http://192.168.50.2/angular-cmf/index.php';
 
-var project = angular.module("project", ["cmf.user", "ngResource"]);
+var project = angular.module("project", ["cmf.user", "ngResource", "cmf.logger"]);
 
 project.config(function ($routeProvider) {
   $routeProvider
-    .when('/login', { controller: 'Login', template: '<status logger="logger"></status><login login="login"></login>' })
+    .when('/login', { controller: 'Login', template: '<h1>Log in</h1><status logger="logger"></status><login login="login"></login>' })
     .when('/', { controller: 'User', templateUrl: "../modules/user/templates/user-admin.html" })
     .when('/user/add', { controller: 'UserAdd', templateUrl: "../modules/user/templates/user-add.html" })
     .when('/user/:id', { controller: 'UserView', templateUrl: "../modules/user/templates/user-view.html" });
 });
 
 project.controller('UserAdd', function ($scope, UserService, TypeService, UserAdd, SessionService, $location) {
-  var session = SessionService.getSession();
+  var session = SessionService.getSession(), Types, User;
   if (!session) {
     $location.path('/login');
   } else {
     // Set up backend url.
-    var Types = TypeService(backendBaseUrl), User = UserService(backendBaseUrl);
+    Types = new TypeService(backendBaseUrl);
+    User = new UserService(backendBaseUrl);
     UserAdd(User, Types, $scope);
   }
 });
 
-project.controller('UserView', function ($scope, $routeParams, UserService, SessionService, $location, UserView, RoleService) {
-  var User = UserService(backendBaseUrl);
-  var Role = RoleService(backendBaseUrl);
-  UserView($routeParams.id, User, Role, $scope);
+project.controller('UserView', function ($scope, $routeParams, UserService, SessionService, $location, UserView, RoleService, Logger) {
+  var User = new UserService(backendBaseUrl), Role = new RoleService(backendBaseUrl);
+  UserView($routeParams.id, User, Role, new Logger(), $scope);
 });
 
 project.controller('User', function ($scope, UserService, Logger, UserAdmin, SessionService, $location) {
